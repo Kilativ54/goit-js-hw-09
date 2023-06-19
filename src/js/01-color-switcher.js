70ac68ef66c61abcad4bd12b83fa505e00fd0aeb
Kilativ54 <vitalikechkalov@gmail.com>
2023-06-19 23:48:40 +0200

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const colorBody = document.querySelector('body');
+btnStart.addEventListener('click', startChange);
+function startChange() {
+  btnStart.disabled = true;
+  timerChange = setInterval(() => {
+    colorBody.style.background = getRandomHexColor();
+  }, 1000);
+  btnStart.disabled = true;
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+btnStop.addEventListener('click', () => {
+  clearInterval(timerChange);
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+});
