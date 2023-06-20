562572f800a1821d2ae6d69aabb7d4231eff8580
Kilativ54 <vitalikechkalov@gmail.com>
2023-06-20 22:23:49 +0200

    task2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,31 +1,65 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
 const inputDate = document.querySelector('#datetime-picker');
-const btnChooseDate = document.querySelector('button[data-start]');
+const btnStart = document.querySelector('button[data-start]');
 const days = document.querySelector('span[data-days]');
 const hours = document.querySelector('span[data-hours]');
 const minutes = document.querySelector('span[data-minutes]');
 const seconds = document.querySelector('span[data-seconds]');
-btnChooseDate.disabled = true;
+btnStart.disabled = true;
 const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-      if(selectedDates[0] < new Date()){
-        Notiflix.Notify.failure('Please choose a date in the future');
-      }else{
-        btnChooseDate.disabled = false;
-      }
-    },
-  };
-
-    flatpickr(inputDate, options)
-    btnChooseDate.disabled = true;
-
-btnChooseDate.addEventListener('click', startTime);
-function startTime(){
-
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] <= new Date()) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+      btnStart.disabled = true;
+    } else {
+      btnStart.disabled = false;
+    }
+  },
 };
+
+flatpickr(inputDate, options);
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
+
+btnStart.addEventListener('click', () => {
+  let timeInterval = setInterval(() => {
+    let countdown = new Date(inputDate.value) - new Date();
+    btnStart.disabled = true;
+    if (countdown > 0) {
+      let timeObject = convertMs(countdown);
+      days.textContent = addLeadingZero(timeObject.days);
+      hours.textContent = addLeadingZero(timeObject.hours);
+      minutes.textContent = addLeadingZero(timeObject.minutes);
+      seconds.textContent = addLeadingZero(timeObject.seconds);
+    } else {
+      clearInterval(timeInterval);
+    }
+  }, 1000);
+});
